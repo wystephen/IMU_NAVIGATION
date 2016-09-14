5df6763cb8d59b0d6d068340c0a33c79e2074533
steve <551619855@qq.com>
2016-09-14 16:33:26 +0800

    Add TwoFoot modules.

---
+++ include/IMU_NAVIGATION/TwoFootContaint.h
@@ -0,0 +1,15 @@
+#pragma once
+//Create by steve in 16-9-14 at 下午4:27
+//
+// Created by steve on 16-9-14.
+//
+
+#include <ros/ros.h>
+
+
+#ifndef IMU_NAVIGATION_TWOFOOTCONTAINT_H
+#define IMU_NAVIGATION_TWOFOOTCONTAINT_H
+
+
+
+#endif //IMU_NAVIGATION_TWOFOOTCONTAINT_H
---
+++ src/TwoFootContaint.cpp
@@ -0,0 +1,22 @@
+#pragma once
+//Create by steve in 16-9-14 at 下午4:27
+//
+// Created by steve on 16-9-14.
+//
+
+#include "../include/IMU_NAVIGATION/TwoFootContaint.h"
+
+#include <ros/ros.h>
+
+#include "../include/IMU_NAVIGATION/DataSynchronization.h"
+
+
+int main(int argc,char ** argv)
+{
+    ros::init(argc,argv,"TwoFoot");
+
+    ros::NodeHandle n_handle;
+
+    ros::spin();
+    return 0;
+}
