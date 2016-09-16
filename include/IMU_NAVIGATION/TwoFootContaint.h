71e2175eec4f806b2111091b4d60b9c5746229f6
steve <551619855@qq.com>
2016-09-16 17:04:35 +0800

    Modified the readme.md

--- include/IMU_NAVIGATION/TwoFootContaint.h
+++ include/IMU_NAVIGATION/TwoFootContaint.h
@@ -32,114 +32,19 @@ public:
     TwoFootEkf(SettingPara &para):para_ptr_(&para)
     {
         //Initial the filter.
-
-        /////////////////////////////////////////////////////////
-        // Initial P
-//        for(int i(0);i<18;++i)
-//        {
-//            for(int j(0);j<18;++j)
-//            {
-//                P_(i,j) = 0;
-//            }
-//        }
-        ZEROLIZEMATRIX(P_);
-
-        //System number 1.
-        for(int i(0);i<3;++i)
-        {
-            P_(i,i) = std::sqrt(para_ptr_->sigma_initial_pos1_(i));
-        }
-
-        for(int i(3);i<6;++i)
-        {
-            P_(i,i) = std::sqrt(para_ptr_->sigma_initial_vel1_(i-3));
-        }
-
-        for(int i(6);i<9;++i)
-        {
-            P_(i,i) = std::sqrt(para_ptr_->sigma_initial_att1_(i-6));
-        }
-
-        //System number 2.
-        for(int i(9);i<12;++i)
-        {
-            P_(i,i) = std::sqrt(para_ptr_->sigma_initial_pos2_(i-9));
-        }
-
-        for(int i(12);i<15;++i)
-        {
-            P_(i,i) = std::sqrt(para_ptr_->sigma_initial_vel2_(i-12));
-        }
-
-        for(int i(15);i<18;++i)
-        {
-            P_(i,i) = std::sqrt(para_ptr_->sigma_initial_att2_(i-15));
-        }
-
-        /////////////////////////////////////////////////
-
-        //R1,R2,R12
-        ZEROLIZEMATRIX(R1_)
-        ZEROLIZEMATRIX(R2_)
-        ZEROLIZEMATRIX(R12_)
-
-
-        for(int i(0);i<3;++i)
-        {
-            R1_(i,i) = std::sqrt(para_ptr_->sigma_vel_(i));
-            R2_(i,i) = std::sqrt(para_ptr_->sigma_vel_(i));
-        }
-
-        R12_.block(0,0,3,3) = R1_;
-
-        R12_.block(3,3,3,3) = R2_;
-
-        //Q
-        ZEROLIZEMATRIX(Q_)
-
-        for(int i(0);i<3;++i)
-        {
-            Q_(i,i) = std::sqrt(para_ptr_->sigma_acc_(i));
-        }
-
-        for(int i(3);i<6;++i)
-        {
-            Q_(i,i) = std::sqrt(para_ptr_->sigma_gyro_(i-3));
-        }
-
-        for(int i(6);i<9;++i)
-        {
-            Q_(i,i) = std::sqrt(para_ptr_->sigma_acc_(i-6));
-        }
-
-        for(int i(9);i<12;++i)
-        {
-            Q_(i,i) = std::sqrt(para_ptr_->sigma_gyro_(i-9));
-        }
-
-        //H1 H2
-        ZEROLIZEMATRIX(H1_)
-        ZEROLIZEMATRIX(H2_)
-        ZEROLIZEMATRIX(H12_)
-
-
-
-        H1_.block(0,3,3,3) = Eigen::Matrix3d::Identity();
-
-        H2_.block(0,12,3,3) = Eigen::Matrix3d::Identity();
-
-        H12_.block(0,3,3,3) = Eigen::Matrix3d::Identity();
-        H12_.block(3,12,3,3) = Eigen::Matrix3d::Identity();
-
-
+        Initial();
 
     }
+
 
 
 protected:
 
     SettingPara* para_ptr_;//Use the pointer point to a global value .
 
+    Eigen::VectorXf x_h_ = Eigen::VectorXf(18);
+
+    Eigen::Matrix<double,18,18> Id_;
 
     //Value use for filter.
     Eigen::Matrix<double,18,18> P_;
@@ -156,11 +61,120 @@ protected:
 
 
 
+    /*
+     * Initialization.
+     */
+    bool Initial();
+
+
+
 
 
 private:
 
 
 };
+
+bool TwoFootEkf::Initial() {
+    ZEROLIZEMATRIX(P_);
+
+    //System number 1.
+    for(int i(0);i<3;++i)
+    {
+        P_(i,i) = std::sqrt(para_ptr_->sigma_initial_pos1_(i));
+    }
+
+    for(int i(3);i<6;++i)
+    {
+        P_(i,i) = std::sqrt(para_ptr_->sigma_initial_vel1_(i-3));
+    }
+
+    for(int i(6);i<9;++i)
+    {
+        P_(i,i) = std::sqrt(para_ptr_->sigma_initial_att1_(i-6));
+    }
+
+    //System number 2.
+    for(int i(9);i<12;++i)
+    {
+        P_(i,i) = std::sqrt(para_ptr_->sigma_initial_pos2_(i-9));
+    }
+
+    for(int i(12);i<15;++i)
+    {
+        P_(i,i) = std::sqrt(para_ptr_->sigma_initial_vel2_(i-12));
+    }
+
+    for(int i(15);i<18;++i)
+    {
+        P_(i,i) = std::sqrt(para_ptr_->sigma_initial_att2_(i-15));
+    }
+
+    /////////////////////////////////////////////////
+
+    //R1,R2,R12
+    ZEROLIZEMATRIX(R1_)
+    ZEROLIZEMATRIX(R2_)
+    ZEROLIZEMATRIX(R12_)
+
+
+    for(int i(0);i<3;++i)
+    {
+        R1_(i,i) = std::sqrt(para_ptr_->sigma_vel_(i));
+        R2_(i,i) = std::sqrt(para_ptr_->sigma_vel_(i));
+    }
+
+    R12_.block(0,0,3,3) = R1_;
+
+    R12_.block(3,3,3,3) = R2_;
+
+    //Q
+    ZEROLIZEMATRIX(Q_)
+
+    for(int i(0);i<3;++i)
+    {
+        Q_(i,i) = std::sqrt(para_ptr_->sigma_acc_(i));
+    }
+
+    for(int i(3);i<6;++i)
+    {
+        Q_(i,i) = std::sqrt(para_ptr_->sigma_gyro_(i-3));
+    }
+
+    for(int i(6);i<9;++i)
+    {
+        Q_(i,i) = std::sqrt(para_ptr_->sigma_acc_(i-6));
+    }
+
+    for(int i(9);i<12;++i)
+    {
+        Q_(i,i) = std::sqrt(para_ptr_->sigma_gyro_(i-9));
+    }
+
+    //H1 H2
+    ZEROLIZEMATRIX(H1_)
+    ZEROLIZEMATRIX(H2_)
+    ZEROLIZEMATRIX(H12_)
+
+
+
+    H1_.block(0,3,3,3) = Eigen::Matrix3d::Identity();
+
+    H2_.block(0,12,3,3) = Eigen::Matrix3d::Identity();
+
+    H12_.block(0,3,3,3) = Eigen::Matrix3d::Identity();
+    H12_.block(3,12,3,3) = Eigen::Matrix3d::Identity();
+
+
+    //Identity matrix
+    ZEROLIZEMATRIX(Id_)
+
+    for(int i(0);i<Id_.rows();++i)
+    {
+        Id_(i,i) = 1.0;
+    }
+
+    return true;
+}
 
 #endif //IMU_NAVIGATION_TWOFOOTCONTAINT_H
