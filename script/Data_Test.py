466982aa79e494e2cac72ab205da513b4cc7785e
steve <551619855@qq.com>
2016-09-26 16:24:51 +0800

    Different file director in different operator.

--- script/Data_Test.py
+++ script/Data_Test.py
@@ -5,7 +5,7 @@ import numpy as np
 
 import scipy as sp
 
-import ros
+# import ros
 
 import matplotlib.pylab as plt
 
@@ -19,11 +19,17 @@ import mpl_toolkits.mplot3d
 if __name__ == '__main__':
 
     # Load data
-    u1 = np.loadtxt("../Data/u1.csv", dtype=float, delimiter=",")
-    u2 = np.loadtxt("../Data/u2.csv", dtype=float, delimiter=",")
+    # u1 = np.loadtxt("../Data/u1.csv", dtype=float, delimiter=",")
+    # u2 = np.loadtxt("../Data/u2.csv", dtype=float, delimiter=",")
+    #
+    # zupt1 = np.loadtxt("../Data/zupt1.csv", dtype=int, delimiter=",")
+    # zupt2 = np.loadtxt("../Data/zupt2.csv", dtype=int, delimiter=",")
 
-    zupt1 = np.loadtxt("../Data/zupt1.csv", dtype=int, delimiter=",")
-    zupt2 = np.loadtxt("../Data/zupt2.csv", dtype=int, delimiter=",")
+    u1 = np.loadtxt("..\\Data\\u1.csv", dtype=float, delimiter=",")
+    u2 = np.loadtxt("..\\Data\\u2.csv", dtype=float, delimiter=",")
+
+    zupt1 = np.loadtxt("..\\Data\\zupt1.csv", dtype=int, delimiter=",")
+    zupt2 = np.loadtxt("..\\Data\\zupt2.csv", dtype=int, delimiter=",")
 
     # u2 = np.loadtxt("../Data/u1.csv", dtype=float, delimiter=",")
     # u1 = np.loadtxt("../Data/u1.csv", dtype=float, delimiter=",")
